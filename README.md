88a51e801a8debe00e3f9767055615b72d736ebf
ohangelina <ohangelina@users.noreply.github.com>
2019-10-24 19:00:27 -0400

    intial commit, website not yet complete

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portolio-Website
+This is my online portfolio website that gives an overview of me and my work. 
---
+++ front-end/html/js/portfolio.js
@@ -0,0 +1,35 @@
+// /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navigation").style.top = "0";
+//   } else {
+//     document.getElementById("navigation").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+// didn't work
+
+(function ($) {
+  $(document).ready(function(){
+
+    // hide .navbar first
+    $("#navigation").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+                 // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 100) {
+                $('.navbar').fadeIn();
+            } else {
+                $('.navbar').fadeOut();
+            }
+        });
+    });
+
+});
+  }(jQuery));

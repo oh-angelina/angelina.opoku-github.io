d4281821da1f53ea065d1fb472f59d97c69006d5
ohangelina <ohangelina@users.noreply.github.com>
2019-11-03 14:33:07 -0500

    Edited welcome banner and added icons

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,35 +1,14 @@
-// /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navigation").style.top = "0";
-//   } else {
-//     document.getElementById("navigation").style.top = "-50px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
 
-// didn't work
 
-(function ($) {
-  $(document).ready(function(){
-
-    // hide .navbar first
-    $("#navigation").hide();
-
-    // fade in .navbar
-    $(function () {
-        $(window).scroll(function () {
-
-                 // set distance user needs to scroll before we start fadeIn
-            if ($(this).scrollTop() > 100) {
-                $('.navbar').fadeIn();
-            } else {
-                $('.navbar').fadeOut();
-            }
-        });
-    });
-
+// Activates Tooltiips on welcome page icons 
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
 });
-  }(jQuery));
+
+
+// Keeps the year in the footer up to date
+var today = new Date();
+var year = today.getFullYear();
+
+var el = document.getElementById("footer");
+el.innerHTML = "<p> &copy;" + year + " Angelina Opoku. All rights reserved.</p>"; 
